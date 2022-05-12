c0d5edce7048ee06414553848905461fc9accb52
Kwame Obeng Yeboah <kwameobengyeboah@live.com>
2022-05-12 06:14:11 +0300

    netflix show completed

--- objects-netflix-tv-shows/js/main.js
+++ objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,26 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
+class MakeNetflixTVShows {
+  constructor(title, genre, rating, numOfEp) {
+    this.title = title;
+    this.genre = genre;
+    this.rating = rating;
+    this.numOfEp - numOfEp;
+  }
+  play() {
+    console.log(`playing`);
+  }
+  stop() {
+    console.log(`stopping...`);
+  }
+  saveToList() {
+    console.log("save to list");
+  }
+}
 
+let bridgerton = new MakeNetflixTVShows(
+  "Bridgerton",
+  "Period Romantic Drama",
+  "99%",
+  16
+);
